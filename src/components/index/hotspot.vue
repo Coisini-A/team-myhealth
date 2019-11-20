f8ae966426f627759e8cb1a5e0c6982dabea6cbd
coisini <206366795@qq.com>
2019-11-21 00:41:03 +0800

    问诊

---
+++ src/components/index/hotspot.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="indexHotspot">
+    热推
+  </div>
+</template>
+
+<script>
+export default {
+  
+}
+</script>
+<style  scoped>
+.indexHotspot{
+  margin: 0.05rem 0;
+  width: 100%;
+  padding: 0.1rem;
+  box-sizing: border-box;
+  border-radius: 0.05rem;
+  background-color: white;
+  box-shadow: 0 0 0.01rem gray;
+}
+</style>
---
+++ src/components/index/search.vue
@@ -0,0 +1,30 @@
+<template>
+    <van-search
+      v-model="value"
+      placeholder="请输入搜索关键词"
+      show-action
+      @search="onSearch"
+      @cancel="onCancel"
+    />
+</template>
+<script>  
+import { Search } from "vant";
+export default {
+  components:{
+    [Search.name]:Search
+  },
+  data() {
+    return {
+      value:""
+    }
+  },
+  methods: {
+    onSearch(){
+      
+    },
+    onCancel(){
+
+    }
+  },
+};
+</script>
