b6c7f93c39af3098dbc3009e89bfa55405592c55
coisini <206366795@qq.com>
2019-11-22 15:49:18 +0800

    11.22

--- src/pages/inquiry.vue
+++ src/pages/inquiry.vue
@@ -1,14 +1,23 @@
 <template>
   <div>
     <!-- <nav-bar></nav-bar> -->
+    <inquiry-header></inquiry-header>
+    <inquiry-content></inquiry-content>
+    <appoIntmen></appoIntmen>
   </div>
 </template>
 <script>
     // import NavBar from "../common/nav-bar"
+    import AppoIntment from "@/components/inquiry/appointment";
+    import InquiryContent from "@/components/inquiry/inquiry-content";
+    import InquiryHeader from "@/components/inquiry/inquiry-header";
     export default {
         name:"forum.vue",
         components:{
             // NavBar
+            "inquiry-header":InquiryHeader,
+            "inquiry-content":InquiryContent,
+            "appoIntment":AppoIntment
         }
     }
 </script>
